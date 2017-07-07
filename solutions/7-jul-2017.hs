284fbe24ab45ddaa4bf4871202b0b2d1930fbc1a
Brian Jing <brian.jing@myob.com>
2017-07-07 15:42:51 +1000

    Consolidate solution to 7-jul-2017

--- solutions/7-jul-2017.hs
+++ solutions/7-jul-2017.hs
@@ -7,24 +7,20 @@ type CoinBag = [Int]
 type Combination = [Int]
 
 countChange :: Money -> CoinBag -> Int
-countChange target coins = getResult target $ count [[]] target coins
+countChange target coins = getResult $ count target coins [[]]
+  where
+    getResult = length . filter (== target) . fmap sum
 
-count :: [Combination] -> Money -> CoinBag -> [Combination]
-count combos target coins
-  | processingComplete target combos = combos
-  | otherwise = count (dedup (combos >>= addCoin target coins)) target coins
-
-addCoin :: Money -> CoinBag -> Combination -> [Combination]
-addCoin target coins combo
-  | sum combo < target = [ sort (coin : combo) | coin <- coins ]
-  | otherwise = [combo]
-
--- Utilities
-processingComplete :: Money -> [Combination] -> Bool
-processingComplete target = not . any (<target) . fmap sum
-
-getResult :: Money -> [Combination] -> Int
-getResult target = length . filter (== target) . fmap sum
+count :: Money -> CoinBag -> [Combination] -> [Combination]
+count target coins combos
+  | processingComplete combos = combos
+  | otherwise = count target coins (dedup (combos >>= addCoin))
+  where
+    addCoin :: Combination -> [Combination]
+    addCoin combo
+      | sum combo < target = [ sort (coin : combo) | coin <- coins ]
+      | otherwise = [combo]
+    processingComplete = not . any (<target) . fmap sum
 
 dedup :: Ord a => [a] -> [a]
 dedup = toList . fromList
